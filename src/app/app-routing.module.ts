bcd12af9025d802825e457b74ee4a40b02d0bc75
lughjw <lughjw@gmail.com>
2021-05-05 23:24:40 -0500

    remove default app html. add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IdCardComponent } from './id-card/id-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "idcard", component: IdCardComponent},
+  {path: "**", redirectTo: "/idcard", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
