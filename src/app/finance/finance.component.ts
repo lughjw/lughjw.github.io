7eca0cbd369410255b91c6080ae3a19b48e02d54
--global <lughjw@gmail.com>
2021-08-02 22:54:28 -0500

    fixed formulas. Added Interest break down.

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -8,20 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class FinanceComponent implements OnInit {
   principal: number = 1;
   interestRate: number = 0.1;
-  compoundsPerCycle: number = 0;
-  cycleCount: number = 12;
+  compoundsPerPeriod: number = 12;
+  periodCount: number = 1;
   cyclyPayment: number = 0;
   total: number = 0;
-  totalFromInterest: number = 0;
+  totalFromPeriodicPayments: number = 0;
+  totalInterest: number = 0;
+  totalInterestFromPayments: number = 0;
+  totalInterestFromPrincipal: number = 0;
   totalFromPrincipal: number = 0;
   guide_url="https://angular.io/guide/forms"
   constructor() { }
 
   onSubmit() { 
     // P(1+r/n)^(nt)
-    this.totalFromInterest = this.cyclyPayment*((Math.pow((1+(this.interestRate/this.compoundsPerCycle)),this.compoundsPerCycle*this.cycleCount)-1)/(this.interestRate/this.compoundsPerCycle));
-    this.totalFromPrincipal = this.principal * Math.pow((1+(this.interestRate/this.compoundsPerCycle)),this.compoundsPerCycle*this.cycleCount);
-    this.total = this.totalFromInterest + this.totalFromPrincipal;
+    let compound = Math.pow((1+(this.interestRate/this.compoundsPerPeriod)),this.compoundsPerPeriod*this.periodCount);
+
+    this.totalFromPeriodicPayments = this.cyclyPayment * this.periodCount*this.compoundsPerPeriod;
+    this.totalInterest = this.cyclyPayment*((compound-1)/(this.interestRate/this.compoundsPerPeriod));
+    this.totalInterestFromPayments = this.totalInterest - this.totalFromPeriodicPayments;
+    this.totalFromPrincipal = this.principal * compound;
+    this.totalInterestFromPrincipal = this.principal * compound - this.principal;
+    this.total = this.totalInterest + this.totalFromPrincipal;
   }
   ngOnInit(): void {
   }
